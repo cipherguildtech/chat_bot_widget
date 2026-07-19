31455b40c3d0d397e1ea8817b255fe1b8ea8a152
Balasubramanian <123bala2006@gmail.com>
2026-07-19 21:11:44 +0530

    vite cofig.ts and test.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "ChatBotWidget",
+      fileName: () => "widget.js",
+      formats: ["iife"],
+    },
+    cssCodeSplit: false,
+    emptyOutDir: true,
+  },
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
