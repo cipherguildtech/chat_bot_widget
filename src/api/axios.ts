847d68dc3c327fe3d7ec2bcacc15a650463bf67e
Karthikeyan <karthikeyan2642005@gmail.com>
2026-08-04 22:41:53 +0530

    update axios baseURL to use dev tunnel endpoint

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
     // baseURL: 'https://prn75417-8000.inc1.devtunnels.ms',
     // baseURL: 'https://7gv7627j-8000.inc1.devtunnels.ms',
-    baseURL: 'https://ragchatbot-production-0643.up.railway.app',
-    // baseURL: 'http://127.0.0.1:8000',
+    //baseURL: 'https://ragchatbot-production-0643.up.railway.app',
+    baseURL: 'https://7gv7627j-8000.inc1.devtunnels.ms/',
     headers: {
         "Content-Type": "application/json",
     },
