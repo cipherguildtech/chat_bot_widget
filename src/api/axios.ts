b7ffaa0cbdce0c63ff50fe050c1eb6917e07e0dd
Balasubramanian <123bala2006@gmail.com>
2026-08-06 21:38:59 +0530

    api changed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,7 @@ const api = axios.create({
     // baseURL: 'https://prn75417-8000.inc1.devtunnels.ms',
     // baseURL: 'https://7gv7627j-8000.inc1.devtunnels.ms',
     baseURL: 'https://ragchatbot-production-0643.up.railway.app',
+    // baseURL: 'https://ragchatbot-production-0643.up.railway.app',
     // baseURL: 'http://127.0.0.1:8000',
     headers: {
         "Content-Type": "application/json",
